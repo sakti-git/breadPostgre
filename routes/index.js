bd129b7d0049b48baf4cea51e91990eb73801d4a
Sakti <firmansyah.sakti14@gmail.com>
2020-09-01 13:44:20 +0700

    Fourth Commit

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 var express = require('express');
+const {
+  json
+} = require('express');
+const {
+  off
+} = require('../app');
 var router = express.Router();
 
 module.exports = function (pool) {
@@ -8,40 +14,44 @@ module.exports = function (pool) {
     let params = [];
     let search = false;
 
-    if (req.query.checkId && req.query.id) {
+    if (req.query.cekId && req.query.id) {
       params.push(`id = ${req.query.id}`);
       search = true;
     }
 
-    if (req.query.checkString && req.query.string) {
-      params.push(`string = "${req.query.string}"`);
+    if (req.query.cekString && req.query.string) {
+      params.push(`string like '%${req.query.string}%'`);
       search = true;
     }
 
-    if (req.query.checkInteger && req.query.integer) {
-      params.push(`integer = "${req.query.integer}"`);
+    if (req.query.cekInteger && req.query.integer) {
+      params.push(`integer = '${req.query.integer}'`);
       search = true;
     }
 
-    if (req.query.checkFloat && req.query.float) {
-      params.push(`float = "${req.query.float}"`);
+    if (req.query.cekFloat && req.query.float) {
+      params.push(`float = '${req.query.float}'`);
       search = true;
     }
 
-    if (req.query.checkDate && req.query.startDate && req.query.endDate) {
-      params.push(`date BETWEEN "${req.query.startDate}" AND "${req.query.endDate}"`);
+    if (req.query.cekDate && req.query.startDate && req.query.endDate) {
+      params.push(`date BETWEEN '${req.query.startDate}' AND '${req.query.endDate}'`);
       search = true;
     }
 
     if (req.query.checkBoolean && req.query.boolean) {
-      params.push(`boolean = "${req.query.boolean}"`);
+      params.push(`boolean = '${req.query.boolean}'`);
       search = true;
     }
-
+    
+    
     let dataSearch = ""
     if (search) {
       dataSearch += `WHERE ${params.join(' AND ')}`
     }
+
+    console.log(params)
+    console.log('test', dataSearch);
 
     const page = Number(req.query.page) || 1;
     const limit = 3;
@@ -57,7 +67,7 @@ module.exports = function (pool) {
         const total = Number(data.rows[0].total);
         const pages = Math.ceil(total / limit);
 
-        let sql = `SELECT * FROM test ${dataSearch} ORDER BY id LIMIT $1 OFFSET $2`;
+        let sql = `SELECT * FROM test ${dataSearch} ORDER BY id LIMIT $1 OFFSET $2`
         pool.query(sql, [limit, offset], (err, data) => {
           if (err) {
             return res.send(err);
@@ -74,9 +84,9 @@ module.exports = function (pool) {
   });
 
   router.post('/', function (req, res, next) {
-    pool.query('INSERT INTO test (string, integer, float, date, boolean) VALUES ($1, $2, $3, $4, $5)', [req.body.string, Number(req.body.integer), Number(req.body.float), req.body.date, req.body.boolean], (err, data) => {
+    pool.query('INSERT INTO test (string, integer, float, date, boolean) VALUES ($1, $2, $3, $4, $5)', [req.body.string, Number(req.body.integer), Number(req.body.float), req.body.date, JSON.parse(req.body.boolean)], (err, data) => {
       if (err) return res.status(500).json({ err })
-      res.json(data)
+      res.json(data.rows)
     })
   });
 
@@ -88,16 +98,16 @@ module.exports = function (pool) {
   });
 
   router.put('/:id', function (req, res, next) {
-    pool.query('UPDATE test SET string=$1, integer=$2, float=$3, date=$4, boolean=$5 WHERE id=$6', [req.body.string, Number(req.body.integer), Number(req.body.float), req.body.date, req.body.boolean, Number(req.params.id)], (err, data) => {
+    pool.query('UPDATE test SET string=$1, integer=$2, float=$3, date=$4, boolean=$5 WHERE id=$6', [req.body.string, Number(req.body.integer), parseFloat(req.body.float), req.body.date, JSON.parse(req.body.boolean), Number(req.params.id)], (err, data) => {
       if (err) return res.json(err)
-      res.json(data)
+      res.json(data.rows)
     })
   });
 
   router.delete('/:id', function (req, res, next) {
     pool.query('DELETE FROM test WHERE id=$1', [Number(req.params.id)], (err, data) => {
       if (err) return res.json(err)
-      res.json(data)
+      res.json(data.rows)
     })
   });
 
